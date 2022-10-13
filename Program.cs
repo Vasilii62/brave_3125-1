af4554abcbf39956e9da394ed687a6283e6ab96d
email <vassili642@gmail.com>
2022-10-13 19:28:48 +0300

    rite ello orld

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2 Hello, World!");
+// void Digits()
+// {
+   // int x = new Random(()).Next(100,999);
+   // int x = 
+// }
